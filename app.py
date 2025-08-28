b6cb67ed8dca65d068dcc76a4702ae3eaea36fc0
Vani-Kandasamy <vani.kandasamy@pyxeda.ai>
2025-08-28 11:24:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,9 @@ CLASS_LABEL.sort()
 
 
 @st.cache_resource
-def get_MobileNetV2_model():
-    base_model = keras.applications.MobileNetV2(include_top=False, weights="imagenet", input_shape=(224, 224, 3), classes=1000, classifier_activation="softmax")
+def get_ConvNeXtXLarge_model():
+    
+    base_model = keras.applications.ConvNeXtXLarge(include_top=False, weights="imagenet", input_shape=(224, 224, 3), classes=1000, classifier_activation="softmax")
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
     model_frozen = Model(inputs=base_model.input, outputs=x)
@@ -50,26 +51,25 @@ def featurization(image_path, model):
 
 
 
-MobileNetV2_featurize_model = get_MobileNetV2_model()
-classification_model = load_sklearn_models("best_ml_model")
+ConvNeXtXLarge_featurize_model = get_ConvNeXtXLarge_model()
+regression_model = load_sklearn_models("Best_MLP_Regressor")
 
 
 def run_app():
-    st.title("Wildfire Prediction")
-    st.subheader("Predict whether there is a chance of your satellite image's forest to have a wildfire")
-    st.markdown("We use a comprehensive model with a 97 percent accuracy to ensure that your photo is correctly predicted to have a chance of a wildfire or not, completely for free.")
-    st.image(IMAGE_ADDRESS, caption="Wildfire Classification Satellite Images")
-    st.subheader("Please upload your satellite image")
-    image = st.file_uploader("Please upload your satellite image", type=["jpg", "png", "jpeg"], accept_multiple_files=False, help="Upload an Image")
+    st.title("Bone Age Prediction")
+    st.subheader("Predict the bone age")
+    st.image(IMAGE_ADDRESS, caption="Bone Age")
+    st.subheader("Please upload your image")
+    image = st.file_uploader("Please upload your image", type=["jpg", "png", "jpeg"], accept_multiple_files=False, help="Upload an Image")
     if image:
         user_image = Image.open(image)
         user_image.save(IMAGE_NAME)
         st.image(user_image, caption="User uploaded an image")
         with st.spinner("Processing..."):
-            image_features=featurization(IMAGE_NAME, MobileNetV2_featurize_model) 
-            model_predict=classification_model.predict(image_features)
-            result_label=CLASS_LABEL[model_predict[0]]
-            st.success(f"Prediction: {result_label}")
+            image_features=featurization(IMAGE_NAME, ConvNeXtXLarge_featurize_model) 
+            model_predict=regression_model.predict(image_features)
+            result=model_predict[0]
+            st.success(f"Prediction: {result}")
 
 
     
